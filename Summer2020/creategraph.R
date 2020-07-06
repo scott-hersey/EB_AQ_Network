faffe4138400893bf1173abc20473e0c05d03497
AllisonBusa <allison.busa@students.olin.edu>
2020-07-06 14:03:57 -0400

    adding work on creating df and including model data

--- Summer2020/creategraph.R
+++ Summer2020/creategraph.R
@@ -1,6 +1,6 @@
-creategraph <- function(sn){
+creategraph <- function(sn, date, plot1, plot2, plot3a, plot3b){
   #creates graphs for validation
-  #input df must contain these columns : no2, rh_manifold, no, date, filtered
+  #input df must contain names of plot variables as sn45$date, etc
   
   #goes with filtering NO2, July 3 Rmd
   layout(matrix(c(1, 1, 1,1,
@@ -10,23 +10,23 @@ creategraph <- function(sn){
                   3, 3, 3, 3,
                   3,3,3, 3), nrow=6, byrow=TRUE))
   
-  plot(sn$date, sn$no, xlab="")
+  plot(date, plot1, xlab="", type="l")
   
   
-  plot(sn$date, sn$rh_manifold, xlab="")
+  plot(date, plot2, xlab="", type="l")
   
-  plot(sn$date, sn$no2, axes=F, xlab="", ylab="", type="b",col="black")
+  plot(date, plot3a, axes=F, xlab="", ylab="", type="l",col="black")
   axis(2, col="black")
   mtext("NO2 ",side=2,line=2.5)
   box()
   
   par(new=T)
   
-  plot(sn$date, sn$filtered,xlab="", ylab="",  axes=F, type="l", col="red")
+  plot(date, plot3b ,xlab="", ylab="",  axes=F, type="l", col="red")
   mtext("Filtered",side=4,col="red",line=2.5)
   axis(4, col="red",col.axis="red")
   
   
-  axis(2,pretty(range(time),10))
+  axis(2,pretty(range(date),10))
   mtext("Time",side=1,col="black",line=2.5)
 }
