cf89bec97618a3d5e979dadb22a9cffa99c35bcc
AllisonBusa <allison.busa@students.olin.edu>
2021-06-18 04:38:54 -0400

    adding boxplot function

---
+++ functions/calc_stat.R
@@ -0,0 +1,8 @@
+calc_stat <- function(x) {
+  # coef <- 1.5
+  # n <- sum(!is.na(x))
+  # calculate quantiles
+  stats <- quantile(x, probs = c(0.1, 0.25, 0.5, 0.75, 0.9))
+  names(stats) <- c("ymin", "lower", "middle", "upper", "ymax")
+  return(stats)
+}
